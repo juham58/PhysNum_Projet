c4c278d54f0918315f46f9bd22f2ed5e17e341b2
juham58 <59977027+juham58@users.noreply.github.com>
2020-05-04 23:42:55 -0400

    rotation de la terre

--- Grid_2D_Equilibrium.py
+++ Grid_2D_Equilibrium.py
@@ -28,13 +28,14 @@ def grid_fct(N, fct):
 def grid_mouton(N, equilib, sm, masse_astre):
     lon = np.linspace(-1.0 * np.pi, 1.0 * np.pi, N)
     lat = np.linspace(-0.5 * np.pi, 0.5 * np.pi, N)
-    theta, delta = np.meshgrid(lon, lat)
     liste = []
     for i in range(len(sm["t"])):
+        theta, delta = np.meshgrid(lon, lat)
+        theta = theta + (2*np.pi/(24*3600))*sm["t"][i]
         G = np.zeros((N, N))
-        print(i)
         G = equilib(sm["L"][i, 0], sm["L"][i, 1], sm["L"][i, 2], sm["S"][i, 0], sm["S"][i, 1], sm["S"][i, 2], masse_astre, theta, delta)
         liste.append(G)
+        print(sm["t"])
     return liste
 
 
@@ -47,4 +48,3 @@ if __name__ == '__main__':
     positions_mars = mouton_3_corps(0, 31 * 24 * 3600, 2000, sys_TMS, F_TMS, slice=6)
     positions_lune = mouton_3_corps(0, 31 * 24 * 3600, 2000, sys_TMS, F_TLS, slice=6)
     a = grid_mouton(50, Equilibrium, positions_mars, m_M)
-    print(a)
