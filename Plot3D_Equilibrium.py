7fe3361c41668504f321f6a0d795ceb57363b807
vincentpiano123 <59986183+vincentpiano123@users.noreply.github.com>
2020-05-04 22:31:53 -0400

    Update Plot3D_Equilibrium.py

--- Plot3D_Equilibrium.py
+++ Plot3D_Equilibrium.py
@@ -1,33 +1,17 @@
 import pyvista as pv
 import numpy as np
-from Grid_2D_Equilibrium import grid_fct, fonc_Equilibrium
-import mpl_toolkits
+from Grid_2D_Equilibrium import grid_mouton
+from saute_mouton import mouton_3_corps, sys_TMS, F_TMS, F_TLS
+from fonctions_marees import Equilibrium
 
-def _cell_bounds(points, bound_position=0.5):
-    """
-    Calculate coordinate cell boundaries.
 
-    Parameters
-    ----------
-    points: numpy.array
-        One-dimensional array of uniformy spaced values of shape (M,)
-    bound_position: bool, optional
-        The desired position of the bounds relative to the position
-        of the points.
+m_T = 5.9722*1e24  # Terre
+m_S = 1.989*1e30  # Soleil
+m_L = 7.349*1e22  # Lune
+m_M = 0.107*m_T  # Mars
 
-    Returns
-    -------
-    bounds: numpy.array
-        Array of shape (M+1,)
 
-    Examples
-    --------
-    >>> a = np.arange(-1, 2.5, 0.5)
-    >>> a
-    array([-1. , -0.5,  0. ,  0.5,  1. ,  1.5,  2. ])
-    >>> cell_bounds(a)
-    array([-1.25, -0.75, -0.25,  0.25,  0.75,  1.25,  1.75,  2.25])
-    """
+def _cell_bounds(points, bound_position=0.1):
     assert points.ndim == 1, "Only 1D points are allowed"
     diffs = np.diff(points)
     delta = diffs[0] * bound_position
@@ -35,41 +19,37 @@ def _cell_bounds(points, bound_position=0.5):
     return bounds
 
 
-# First, create some dummy data
-
-# Approximate radius of the Earth
 RADIUS = 6371.0
-
-# Longitudes and latitudes
-x = np.linspace(-180.0, 180.0, 200)
-y = np.linspace(-90, 90, 200)
-y_polar = 90.0 - y  # grid_from_sph_coords() expects polar angle
-
-xx, yy = np.meshgrid(x, y)
-
-
-fct_Equilibrium = fonc_Equilibrium(384400000.0, 235828.00, 492358.0, 6.4185e23)
-Grid_equilibrium = grid_fct(200, fct_Equilibrium)
+x = np.linspace(-180.0, 180.0, 50)
+y = np.linspace(-90, 91, 50)
+y_polar = 90.0 - y
 
 
 # Create arrays of grid cell boundaries, which have shape of (x.shape[0] + 1)
 xx_bounds = _cell_bounds(x)
 yy_bounds = _cell_bounds(y_polar)
-# Vertical levels
-# in this case a single level slightly above the surface of a sphere
 levels = [RADIUS * 1.01]
 
 
+positions_mars = mouton_3_corps(0, 3 * 24 * 3600, 2000, sys_TMS, F_TMS, slice=8)
+print(positions_mars)
+positions_lune = mouton_3_corps(0, 3 * 24 * 3600, 2000, sys_TMS, F_TLS, slice=8)
+a = grid_mouton(50, Equilibrium, positions_mars, m_M)
+
+
+# Remplir la grille de valeurs scalaires
 grid_scalar = pv.grid_from_sph_coords(xx_bounds, yy_bounds, levels)
+grid_scalar.cell_arrays["Hauteur de la marée (mètres)"] = np.array(a[6]).swapaxes(-2, -1).ravel("C")
+a = grid_scalar
 
-# And fill its cell arrays with the scalar data
-grid_scalar.cell_arrays["Hauteur de la marée (mètres)"] = np.array(Grid_equilibrium[2]).swapaxes(-2, -1).ravel("C")
 
-
+# Fonction qui calcule les coordonnées et retourne un array (x,y,z)
 def points(lon, lat):
     R = RADIUS
     return np.array([1.01*R*np.cos(lat)*np.cos(lon), 1.01*R*np.cos(lat)*np.sin(lon), 1.01*R*np.sin(lat)])
 
+
+# Crée des points sur la sphère
 label = ["Québec", "Paris", "Tokyo", "Brasilia"]
 Quebec = points(-1.2428137, 0.8170563)
 Paris = points(0.0409942, 0.85265268)
